e74c0e1138e31018bdfcf2707d5486fbbc434dbd
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-10-18 19:50:59 +0300

    1_6_NextJs_app metadata

--- src/app/(public)/users/[id]/page.tsx
+++ src/app/(public)/users/[id]/page.tsx
@@ -1,12 +1,19 @@
 import {FC} from "react";
+import {Metadata} from "next";
 
 type Props={
     params:{id:string}
 }
+
+export const generateMetadata=async({params}:Props):Promise<Metadata> => {
+const {id} = await params;
+    return {
+        title: "User Page title"+ id,
+    }
+
+}
 const UserPage:FC<Props> = async ({params}) => {
     const {id} = await params;
-
-
     return (
         <div>
             User Page content {id}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
         <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         root app layout
-
         {children}
         </body>
         </html>
