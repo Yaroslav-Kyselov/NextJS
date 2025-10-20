35baf608f4e99b5f01b0c707469a6f0e08157327
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-10-20 19:37:00 +0300

    2.2 pass data between page

--- src/app/(public)/users/[id]/page.tsx
+++ src/app/(public)/users/[id]/page.tsx
@@ -1,8 +1,10 @@
 import {FC} from "react";
 import {Metadata} from "next";
+import {SearchParams} from "next/dist/server/request/search-params";
 
 type Props={
-    params:{id:string}
+    params:Promise<{id:string}>;
+    searchParams:Promise<SearchParams>;
 }
 
 export const generateMetadata=async({params}:Props):Promise<Metadata> => {
@@ -12,8 +14,12 @@ const {id} = await params;
     }
 
 }
-const UserPage:FC<Props> = async ({params}) => {
+const UserPage:FC<Props> = async ({params,searchParams}) => {
     const {id} = await params;
+    const sp = await searchParams;
+    console.log(sp)
+
+
     return (
         <div>
             User Page content {id}
--- src/components/users/UsersComponent.tsx
+++ src/components/users/UsersComponent.tsx
@@ -9,7 +9,10 @@ export const UsersComponent = async () => {
         <div>
             {
                 users.map(user => <div key={user.id}>
-                <Link href={'/users/'+user.id.toString()}>{user.id} {user.name} </Link>
+                    <Link href={{
+                        pathname: '/users/' + user.id.toString(),
+                        query: {name: user.name}
+                    }}>{user.id} {user.name} </Link>
                 </div>)
             }
         </div>
