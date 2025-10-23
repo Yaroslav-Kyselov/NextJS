5893eb5482f0721b8bb48df00e1803000fd479e4
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-10-23 19:59:12 +0300

    2.3 simple form

--- src/app/(public)/about/page.tsx
+++
@@ -1,9 +0,0 @@
-const AboutPage = () => {
-    return (
-        <div>
-            About Page content
-        </div>
-    )
-}
-
-export default AboutPage;
--- src/services/api.service.tsx
+++
@@ -1,8 +0,0 @@
-import {IUsers} from "@/models/IUsers";
-
-export const getAllUsers = async ():Promise<IUsers[]> => {
-    const users = await fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json());
-    return users;
-
-}
