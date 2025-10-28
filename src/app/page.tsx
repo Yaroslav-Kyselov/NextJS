f1df0786714e0b8d8518898e8d441800d78965a0
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-10-28 19:26:41 +0200

    2.3 server actions and mutations (fin)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import Form from "next/form";
-import {saveMeal} from "@/server-actions/serverActions";
+import {getMeals, saveMeal} from "@/server-actions/serverActions";
 
-export default function Home() {
+export default async function Home() {
+
+    const mealsArray = await getMeals();
+
     return (
         <div>
-           <Form action={saveMeal} >
-               <input type={"text"} name={"title"}/>
+            <Form action={saveMeal}>
+                <input type={"text"} name={"title"}/>
                 <button>send</button>
+            </Form>
+            <div className={'meals-list'}>
+                {mealsArray.map(meal => (
+                    <div key={meal.id}>{meal.id} {meal.title}</div>
+                ))}
 
-
-           </Form>
-
+            </div>
         </div>
-);
+    );
 }
--- src/server-actions/serverActions.tsx
+++ src/server-actions/serverActions.tsx
@@ -3,14 +3,21 @@
 import sql from 'better-sqlite3';
 import {revalidatePath} from "next/cache";
 
-const db=sql('meals.db');
+const db = sql('meals.db');
 
 export const saveMeal = async (formData: FormData) => {
-    // console.log(formData);
-    // console.log("saveAction");
-const titleValue = formData.get('title');
-db.prepare('insert into meals (title) values (?)').run(titleValue);
+    //formData назва довільна, FormData - тип даних (обов'язково)
+
+    //fetch post
+    //do come db stuff
+    //add data to db
+    const titleValue = formData.get('title'); //отримуємо значення з форми по name
+    db.prepare('insert into meals (title) values (?)').run(titleValue);
+    revalidatePath('/'); //шлях до сторінки, яку потрібно оновити
 
 
+}
+export const getMeals=async ():Promise<{id:number, title:string}[]> =>{
 
+    return db.prepare<{id:number, title:string}[]>('select * from meals').all() as {id:number, title:string}[];
 }
